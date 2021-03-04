46889c2f5084987708b439c7defd984a87a54536
PesyCorm <pesycorm@yandex.ru>
2021-03-04 13:43:33 +0300

    code comments

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Скрипт для получения элементов со страницы
+# Предусловие: страница должна быть запущена, каждая функция принимает на вход объект вебдрайвера
+# input_area() возвращает элемент поля ввода
+# get_result_button() возвращает элемент кнопки "Посчитать"
+# result_text() возвращает текст результата
 
 def input_area(driver):
 	return WebDriverWait(driver, 10).until(
--- tests/test_valid_velues.py
+++ tests/test_valid_velues.py
@@ -1,16 +1,21 @@
 import pytest
 from pages import *
 
+# Скрипт для тестирования страницы http://tutu-ru.github.io/
+# При старте открывается браузер (по умолчанию Chrome), переходит на тестируемую страницу
+# В поле ввода даты рождения поочередно вводит даты из параметризации, сравнивая с ожидаемым результатом
+# В случае несовпадения, выводит текст ошибки с датой рождения и ожидаемым результатом
 
 @pytest.mark.parametrize("date, expected_result",
- [("11.01.2000", "21"), ("13.07.1999", "21"), ("20.09.2007", "13")]
- )
+                         [("11.01.2000", "21"), ("13.07.1999", "21"), ("20.09.2007", "13")]
+                         )
 def test_check_results(driver, date, expected_result):
-	inp_obj = input_area(driver)
-	btn_obj = get_result_button(driver)
+    inp_obj = input_area(driver)
+    btn_obj = get_result_button(driver)
 
-	inp_obj.send_keys(date)
-	btn_obj.click()
-	assert expected_result in result_text(driver), f"Ожидалось, что для даты рождения {date} возраст будет равен {expected_result}"
+    inp_obj.send_keys(date)
+    btn_obj.click()
+    assert expected_result in result_text(
+        driver), f"Ожидалось, что для даты рождения {date} возраст будет равен {expected_result}"
 
-	inp_obj.clear()
+    inp_obj.clear()
